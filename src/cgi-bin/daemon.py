9da82c756ca59591a71f6e8dccec545915358ea2
christianroll <christianr@comp.ufscar.br>
2015-10-03 16:41:44 -0300

    retorno ao daemon inicial

--- src/cgi-bin/daemon.py
+++ src/cgi-bin/daemon.py
@@ -10,9 +10,6 @@ from __future__ import division
 from __future__ import print_function
 
 import socket
-import struct
-import sys
-from thread import *
 from subprocess import check_output
 from subprocess import STDOUT
 
@@ -25,45 +22,8 @@ __license__ = "GPL v3"
 __version__ = "1.0"
 
 
-def send_one_message(sock, data):
-    length = len(data)
-    sock.sendall(struct.pack('<H', length))
-    sock.sendall(data)
-
-def recv_one_message(sock):
-    lengthbuf = recvall(sock, 4)
-    length, = struct.unpack('<H',lengthbuf)
-    return recvall(sock, length)
-
-
-def recvall(sock, count):
-    buf = b''
-    while count:
-        newbuf = sock.recv(count)
-        if not newbuf: return None
-        buf += newbuf
-        count -= len(newbuf)
-    return buf
-
-def clientthread(connection,client_address):
-    #infinite loop so that function do not terminate and thread do not end.
-    while True:
-        #Receiving from client
-        cmd=recv_one_message(connection)
-	#if not buf:
-        #     break
-        print("Recebi: {} de {}".format(cmd, client_address))
-        allowed_cmds = ['df', 'finger', 'ps', 'uptime']
-        if cmd in allowed_cmds:
-              saida = check_output(cmd, stderr=STDOUT, shell=True)
-        else:
-              saida = "Comando invalido!"
-        send_one_message(connection, saida)
-    connection.close()
-
-
 # IP e porta da máquina local
-ip = '10.37.129.3'
+ip = '10.0.0.3'
 porta = 10000
 
 
@@ -73,24 +33,24 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind
 server_address = (ip, porta)
-try:
-    sock.bind(server_address)
-except socket.error , msg:
-    print ("Bind failed. Error Code : {} Message: {}".format(str(msg[0]), msg[1]))
-    sys.exit()
+sock.bind(server_address)
 
 
 # Fica na escuta aguardando conexões
-sock.listen(10)
+sock.listen(1)
 
-
-#now keep talking with the client
-while 1:
-    #wait to accept a connection - blocking call
-    connection, client_address = sock.accept()
-     
-    #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
-    start_new_thread(clientthread ,(connection,client_address))
-
-
-
+while True:
+    try:
+        while True:
+            connection, client_address = sock.accept()
+### Após o sock.accepft, a main thread deve iniciar uma nova thread
+            cmd = connection.recv(4096)
+            print("Recebi: {} de {}".format(cmd, client_address))
+            allowed_cmds = ['df', 'finger', 'ps', 'uptime']
+            if cmd in allowed_cmds:
+                saida = check_output(cmd, stderr=STDOUT, shell=True)
+            else:
+                saida = "Comando invalido!"
+            connection.sendall(saida)
+    finally:
+        connection.close()
