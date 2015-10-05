8402634b7191b30e3cc1965addd6b3958026c19e
Julio Silva <julio@juliobs.com>
2015-10-05 00:33:11 -0300

    No more "Address already in use"
    Read more: https://docs.python.org/2/library/socket.html

--- src/cgi-bin/daemon.py
+++ src/cgi-bin/daemon.py
@@ -81,6 +81,7 @@ class Server:
         try:
             print("Opening '{}':'{}'".format(self.host, self.port))
             self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             self.server.bind((self.host, self.port))
             self.server.listen(5)
         except socket.error, (value, message):
